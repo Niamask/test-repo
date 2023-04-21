290b033db38bb548fcbeaa91a9cad8626457bffb
Niamask <niamaskay@gmail.com>
2023-04-21 11:40:20 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 'A';
+
+	while (x <= 'Z')
+	{
+		putchar(tolower(x));
+		x++;
+	}
+	x = 'A';
+	while (x <= 'Z')
+	{
+		putchar(toupper(x));
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
