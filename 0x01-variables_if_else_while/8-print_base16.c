8e91b1b2331e421942137432a8b71c522bc1a4bf
Niamask <niamaskay@gmail.com>
2023-04-21 12:08:08 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+	x = 'A';
+	while (x <= 'F')
+	{
+		putchar(tolower(x));
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
