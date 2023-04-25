be3688679e8923376a4958643475c479ee53bb88
Niamask <niamaskay@gmail.com>
2023-04-25 16:20:30 -0700

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The int to compute its abs
+ *
+ * Return: the absolute value of an integer
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
