d3fd87dcc4a4047082584bc231832ef8a17d529a
Niamask <niamaskay@gmail.com>
2023-04-28 09:26:26 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds two integers and returns the result.
+ * @a : Two character to add
+ * @b : Two character to add
+ * Return: the result of add.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
