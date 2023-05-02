03867c28c53f0d960220154e79875bade08394be
Niamask <niamaskay@gmail.com>
2023-05-02 12:35:33 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s :  is a char
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
