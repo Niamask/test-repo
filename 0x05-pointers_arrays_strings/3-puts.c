59c0d557138c12b762840d2450c0516c79d47a2c
Niamask <niamaskay@gmail.com>
2023-05-02 13:03:01 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _puts - prints a string.
+ * @str : str char
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
