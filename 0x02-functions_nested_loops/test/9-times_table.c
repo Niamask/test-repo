07926c178a2b31e6a125aa6ba85c5172621f40a1
Niamask <niamaskay@gmail.com>
2023-05-04 05:42:26 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/test/9-times_table.c
+++ 0x02-functions_nested_loops/test/9-times_table.c
@@ -7,42 +7,31 @@
  * Return: nothing
  */
 
-void times_table(void)
+int times_table(void)
 {
-	int x = 0;
-
-	while (x <= 9)
-	{
-		int y = 0;
-
-		 while (y <= 9)
-        	{
-                	int z = x * y;
-			
-			if (z > 9)
-			{
-				int ld = z % 10;
-				int fd = z / 10;
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + fd);
-				_putchar('0' + ld);
-			}
-			else if (y > 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + z);
-				
-			}
-			else 
-			{
-				_putchar('0' + z);
-			}
-			y++;
-        	}
-		 _putchar('\n');
-		 x++;
-	}
+	int a[5];
+	int *p;
+	int *p2;
+	
+	*a = 98;
+	*(a + 1) = 198;
+	*(a + 2) = 298;
+       	a[3] = 398;
+	*(a + 4) = 498;
+	printf("Value of a[0]: %d\n", *a);
+	printf("Value of a[1]: %d\n", *(a + 1));
+	printf("Value of a[2]: %d\n", *(a + 2));
+	printf("Value of a[3]: %d\n", *(a + 3));
+	printf("Value of a[4]: %d\n", *(a + 4));
+	p = a + 1;
+	printf("Value of p : %d\n", *p);
+	*p = 98;
+	printf("Value of p : %d\n", *p);
+	printf("Value of a[1]: %d\n", *(a + 1));
+	p2 = a + 3;
+	*p2 = *p + 1337;
+	printf("Value of p2 : %d\n", *p2);
+	printf("Value of a[3]: %d\n", *(a + 3));
+	return (0);
 }
+
