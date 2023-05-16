8aea510fed772aa7b27e0b9a5b3599464b8f86cb
Niamask <niamaskay@gmail.com>
2023-05-16 14:51:58 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,20 +1,13 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	char chaine[] = "_putchar\n";
-	int i = 0;
-
-	while (chaine[i])
-	{
-		_putchar(chaine[i]);
-		i++;
-	}
-	return (0);
+	return (write(1, &c, 1));
 }
