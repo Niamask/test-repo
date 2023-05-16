6e2607aaa8730b06b57802e10c5d5879f0ce41bb
Niamask <niamaskay@gmail.com>
2023-05-16 14:19:42 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,36 @@
+#include <string.h>
+/**
+ * _strstr - a function that locates a substring.
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: 1 or 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+
+			} while (haystack[i] == needle[i]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
+
