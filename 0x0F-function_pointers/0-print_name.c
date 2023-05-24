924c0f56de1222139cf9af4188829b7aba621859
Niamask <niamaskay@gmail.com>
2023-05-24 05:06:45 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_name - prints a name.
+ * @name: name of the person
+ * @f: pointer to a function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
