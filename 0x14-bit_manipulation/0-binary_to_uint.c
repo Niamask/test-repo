4efa7feeb9a025991521a1c0ecd0fbe7f63970ec
Niamask <niamaskay@gmail.com>
2023-06-08 16:42:29 -0700

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,17 @@
  *
  * Return: 0 or converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	long unsigned int i, sum = 0; 
-	/*int x = atoi(b)*/;
+	long unsigned int i, sum = 0;
 
+	/*int x = atoi(b);*/
 	for (i = 0; i < strlen(b); i++)
 	{
-	/*	printf("%c", b[i]);*/
-		sum += atoi(b[i]);
-
-		/*if ( b[i] == 1)
-			sum += pow(2, i);*/
+		if (b[i] == 1)
+			sum += pow(2, i);
 	}
-	
 
 	return (sum);
 }
