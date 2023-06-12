4ab1860f99a6e924bc0b7a5765e8178ec0b29aef
Niamask <niamaskay@gmail.com>
2023-06-12 13:20:03 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,20 +16,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
-	f = open(filename, O_CREAT | O_RDWR, 600);
+	if (text_content == NULL)
+		text_content = "";
 
-	if (f == -1)
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
 		return (-1);
 
-	chmod(filename, 644);
+	write(file, text_content, i);
 
-	if (text_content != NULL)
-		write(f, text_content, strlen(text_content));
-	close(f);
 	return (1);
 }
